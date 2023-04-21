13b8e689418ee54b1b5a6aced1cfef8cff02ccfb
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-04-21 12:58:58 -0400

    Began making loginController
    left off in timestamp 16:34 is tutorial

---
+++ src/JWTApp/JWTApp/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using JWTApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // The reason why using HttpPost is better than using HttpGet for logins is because the data in a HttpPost request is more secure
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+
+            if (user == null)
+                return NotFound("User not found.");
+
+            var token = Generate(user);
+            return Ok(token);
+        }
+
+        private UserModel? Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(x => x.Username.ToLower() 
+                == userLogin.Username.ToLower() && x.Password == userLogin.Password);
+
+            if (currentUser == null)
+                return null;
+
+            return currentUser;
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.Surname),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+        }
+
+    }
+}
